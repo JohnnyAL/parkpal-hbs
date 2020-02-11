1ce29e16f09b992da9346dc33ebda58896871333
Johnny Burke <johnalbertburke@gmail.com>
2020-02-11 16:23:42 +0100

    added parking spot detail page and delete posting logic

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  console.log({ user: req.session.user });
   res.render("index", { user: req.session.user });
 });
 
--- routes/parking-spots.js
+++ routes/parking-spots.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Spot = require("../models/Spot");
 const User = require("../models/User");
 
-router.get("/parking-spots", (req, res) => {
+router.get("/list", (req, res) => {
   Spot.find()
     .then(spots => {
       res.render("parking-spots/list.hbs", { spots, user: req.session.user });
@@ -29,13 +29,8 @@ router.get("/add", loginCheck, (req, res) => {
 //   res.render("parking-spots/add.hbs", { role: req.session.user.role });
 // });
 
-router.post("/parking-spots", loginCheck, (req, res, next) => {
+router.post("/add", loginCheck, (req, res, next) => {
   const { name, address, description, size, type, price } = req.body;
-
-  User.findById({ _id: req.session.user._id }).then(res => {
-    console.log("hello", res);
-  });
-
   Spot.create({
     name,
     address,
@@ -45,13 +40,59 @@ router.post("/parking-spots", loginCheck, (req, res, next) => {
     price,
     owner: req.session.user._id
   })
-    .then(() => {
-      console.log(req.session.user._id);
-      return User.findByIdAndUpdate(req.session.user._id, { role: "host" });
+    .then(createdSpot => {
+      if (req.session.user.role === "basic") {
+        return User.findByIdAndUpdate(
+          req.session.user._id,
+          { role: "host" },
+          { new: true }
+        ).then(updatedUser => {
+          return {
+            user: updatedUser,
+            createdSpot: createdSpot
+          };
+        });
+      }
+      return { user: req.session.user, createdSpot: createdSpot };
     })
+    .then(result => {
+      req.session.user = result.user;
+      res.redirect(`/parking-spots/detail/${result.createdSpot._id}`);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/detail/:id", (req, res, next) => {
+  Spot.findById(req.params.id)
+    // .populate("owner - also can populate reviews here when added. Look at monti's room example for syntax")
+    .populate({
+      path: "owner"
+    })
+    .then(spot => {
+      let showDelete = false;
+      if (
+        req.session.user &&
+        spot.owner._id.toString() === req.session.user._id.toString() //got error in console here
+      ) {
+        showDelete = true;
+      }
+      res.render("parking-spots/detail.hbs", {
+        spot,
+        showDelete: showDelete,
+        user: req.session.user
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/delete/:id", (req, res, next) => {
+  Spot.deleteOne({ _id: req.params.id, owner: req.session.user._id })
     .then(() => {
-      console.log("redirecttttt");
-      res.redirect("parking-spots");
+      res.redirect("/parking-spots/list");
     })
     .catch(err => {
       next(err);
