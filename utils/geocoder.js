f39222d8510ee85c8e3dc45e48b5dae1636078a9
Johnny Burke <johnalbertburke@gmail.com>
2021-08-11 16:52:51 -0700

    unbold text on detail page

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -10,3 +10,17 @@ const options = {
 const geocoder = NodeGeocoder(options);
 
 module.exports = geocoder;
+
+function houseOfCards(floors) {
+  if (Number.isInteger(floors) && floors >= 1) {
+    let numOfCards = 2;
+    let increase = 2;
+    for (let i = 1; i <= floors; i++) {
+      increase += 3;
+      numOfCards += increase;
+    }
+    return numOfCards;
+  } else {
+    throw new Error("Error");
+  }
+}
