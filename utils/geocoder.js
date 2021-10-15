49e5e722141bf8f1c62011fbc581a4db40c21d66
Johnny Burke <johnalbertburke@gmail.com>
2021-10-15 15:49:01 -0700

    edit inputs boxes on search form

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -10,3 +10,16 @@ const options = {
 const geocoder = NodeGeocoder(options);
 
 module.exports = geocoder;
+
+function groupByCommas(n) {
+  let str = n.toString();
+  if (str.length < 4) {
+    return str;
+  } else {
+    let arr = str.split("").reverse();
+    for (let i = 2; i < arr.length; i += 3) {
+      arr.splice(i, 1, "," + arr[i]);
+    }
+    return arr.reverse().join("");
+  }
+}
